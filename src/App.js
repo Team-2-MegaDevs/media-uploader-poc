5e2733ff05c76d2776c069c4d79719c5d7cf8516
Eduard Draghiciu <drag0022@algonquinlive.com>
2022-02-14 18:13:08 -0500

    handle file size

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import "./App.css";
-import { handleUpload } from "./utils/firebase-utils";
+import { useState } from "react";
+import { handleUpload, getFileSize } from "./utils/firebase-utils";
 
 function App() {
+  const [file, setFile] = useState(null);
+  const [isOptimized, setIsOptimized] = useState(false);
+  const [fileSize, setFileSize] = useState(null);
   return (
     <div className="App">
-      <button className="uploadButton" onClick={handleUpload}>
-        Upload File
-      </button>
-      <span className="uploadProgress"></span>
+      <form
+        onSubmit={(ev) => {
+          ev.preventDefault();
+          handleUpload(file);
+        }}
+      >
+        <input
+          className="uploadForm"
+          type="file"
+          accept="image/*, audio/*"
+          onChange={(ev) => {
+            setFile(ev.target.files[0]);
+            setIsOptimized(() => {
+              //only allow files < 10MB to be uploaded
+              return ev.target.files[0].size < 1000000 ? true : false;
+            });
+            setFileSize(getFileSize(ev.target.files[0]));
+          }}
+        ></input>
+        <p className="uploadProgress">
+          Current File Size: {fileSize}.{" "}
+          {isOptimized ? "" : "Please upload a file < 10MB!"}
+        </p>
+        <button type="submit" disabled={!isOptimized}>
+          Upload File
+        </button>
+      </form>
       <p className="downloadLink"></p>
     </div>
   );
--- src/utils/firebase-utils.js
+++ src/utils/firebase-utils.js
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import filesize from "filesize";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: `${process.env.REACT_APP_API_KEY}`,
   authDomain: "speak-habla-poc.firebaseapp.com",
@@ -16,6 +13,14 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export const handleUpload = () => {
-  console.log({ app });
+export const handleUpload = (file) => {
+  //check file size
+  getFileSize(file);
+  console.log({ file });
+};
+
+export const getFileSize = (file) => {
+  if (file) {
+    return filesize(file.size);
+  }
 };
